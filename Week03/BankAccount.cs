09d87f081c8bc7cad30caeb462cdf00b71a14b44
User <User@USER-PC>
2025-02-18 21:32:13 -0800

    update w/ week3 homework assignment

---
+++ Week03/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace Week03;
+
+public class BankAccount
+{
+    private static int s_accountNumberSeed = 1234567890;
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        this.Owner = name;
+        this.Balance = initialBalance;
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+    }
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+    }
+}
---
+++ Week03/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Week03;
+public class Program
+{
+    public static void Main()
+    {
+        var account = new BankAccount("Bob", 1000);
+        Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+    }
+}
